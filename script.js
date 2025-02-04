857e5e7afa91b320e6545392b56b9b9629221b3a
riley-mitchell <riley.mitchell@vhhscougars.org>
2025-02-04 10:44:34 -0600

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,8 @@
-document.querySelectorAll('nav ul li').forEach(item => {
-    item.addEventListener('click', () => {
-        alert(`You clicked on ${item.innerText}`);
+// script.js
+document.querySelectorAll('nav ul li a').forEach(link => {
+    link.addEventListener('click', (event) => {
+        event.preventDefault();
+        const page = event.target.getAttribute('href');
+        window.location.href = page;
     });
 });
